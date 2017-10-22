275d8941601f9ee692a47e6875bcc58ad9763b89
nyancodeid <ryandevstudio@gmail.com>
2017-10-22 17:24:46 +0700

    update

--- static/app.js
+++ static/app.js
@@ -1,27 +1,26 @@
-(function() {
-	var Global = {
-		inc: 0
-	}
-	var Audio = new Media('file://' + Function.getPath() + "media/ka-ching.mp3");
-	var Events = {
-		onDeviceReady: function() {
-			shake.startWatch(Events.onShake, 30);
-		},
-		onShake: function() {
-			Audio.play();
-			Global.inc++;
 
-			document.getElementById('shake').textContent = Global.inc;
-		}
-	}
-	var Function = {
-		getPath: function() {
-			var path = window.location.pathname;
-			path = path.substr( path, path.length - 10 );
-			
-			return path;
-		}
-	}
+var Global = {
+	inc: 0
+}
+var Audio = new Media('file://' + Function.getPath() + "media/ka-ching.mp3");
+var Events = {
+	onDeviceReady: function() {
+		shake.startWatch(Events.onShake, 30);
+	},
+	onShake: function() {
+		Audio.play();
+		Global.inc++;
 
-	document.addEventListener('deviceready', Events.onDeviceReady, false);
-})
+		document.getElementById('shake').textContent = Global.inc;
+	}
+}
+var Function = {
+	getPath: function() {
+		var path = window.location.pathname;
+		path = path.substr( path, path.length - 10 );
+		
+		return path;
+	}
+}
+
+document.addEventListener('deviceready', Events.onDeviceReady, false);
