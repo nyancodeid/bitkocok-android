d7016d72e354516c61d9eb6ed75863cc29855c3d
nyancodeid <ryandevstudio@gmail.com>
2017-10-22 18:05:42 +0700

    update

--- static/app.js
+++ static/app.js
@@ -1,52 +1,16 @@
-alert('loaded');
+var Audio = new Media('file://' + Function.getPath() + "media/ka-ching.mp3");
+var index = 0;
+
 if (cordova) {
-	alert('cordova OK');
-
 	document.addEventListener("deviceready", function() {
-		if (shake) {
-			alert('OK');
-		} else {
-			alert('Shake not ok');
-		}
-		shake.startWatch(Events.onShake, 30);
-	}, false);
+		shake.startWatch(function() {
+			Audio.play();
+			alert('kocok erus');
 
-	if (shake) {
-			alert('OK');
-		} else {
-			alert('Shake not ok');
-		}
-		shake.startWatch(Events.onShake, 30);
+			document.getElementById('shake').innerHTML = index;
+			index++;
+		}, 30);
+	}, false);
 } else {
 	alert('cordova ERR');
 }
-var Global = {
-	inc: 0
-}
-var Audio = new Media('file://' + Function.getPath() + "media/ka-ching.mp3");
-var Events = {
-	onDeviceReady: function() {
-		if (shake || window.shake) {
-			alert('OK');
-		} else {
-			alert('Shake not ok');
-		}
-		shake.startWatch(Events.onShake, 30);
-	},
-	onShake: function() {
-		Audio.play();
-		Global.inc++;
-
-		document.getElementById('shake').innerHTML = Global.inc;
-	}
-}
-var Function = {
-	getPath: function() {
-		var path = window.location.pathname;
-		path = path.substr( path, path.length - 10 );
-		
-		return path;
-	}
-}
-
-
