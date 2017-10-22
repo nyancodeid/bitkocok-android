2136a9226a3100cd2fb86e38d5abdfc5cf45bc21
nyancodeid <ryandevstudio@gmail.com>
2017-10-22 17:38:59 +0700

    update

--- static/app.js
+++ static/app.js
@@ -1,11 +1,16 @@
 alert('loaded');
+if (cordova) {
+	alert('cordova OK');
+} else {
+	alert('cordova ERR');
+}
 var Global = {
 	inc: 0
 }
 var Audio = new Media('file://' + Function.getPath() + "media/ka-ching.mp3");
 var Events = {
 	onDeviceReady: function() {
-		if (shake) {
+		if (shake || window.shake) {
 			alert('OK');
 		} else {
 			alert('Shake not ok');
